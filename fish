d7b396751d533eef0db631ed3df17256d403b533
jolee18 <jolee18@student.sdu.dk>
2024-03-23 01:06:10 +0100

    added neofetch, nvim, tmux

--- fish
+++
@@ -1,1 +0,0 @@
-/home/joakim/.config/fish/
---
+++ fish/fish/config.fish
@@ -0,0 +1,24 @@
+if status is-interactive
+    # Commands to run in interactive sessions can go here
+  alias vim=nvim
+  tmux
+  cd
+  clear
+end
+
+function gitgap
+    set message (string join " " $argv)
+    if test -z "$message"
+        set message "added stuff"
+    end
+    git add . 
+    git commit -m "$message"
+    git push
+    echo "Changes committed with message: '$message' and pushed to remote."
+end
+
+function mdcd 
+  mkdir $argv 
+  cd $argv
+end 
+
